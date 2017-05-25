fa2a443f5f376935f643ec6835e14216598a0078
Aspen Humphrey <aspenjadedimmick@gmail.com>
2017-05-25 13:46:51 -0500

    first commitment

---
+++ for-loops.js
@@ -0,0 +1,15 @@
+
+for (var  i = 5; i <= 120; i += 10){
+	console.log(i);
+}
+
+for (var i = 4096; i >= 1; i = i/2) {
+	console.log(i);
+}
+
+
+var americanPresidents = ['georgeWashington' , 'johnAdams' , 'thomasJefferson'];
+for ( var i = 0; i < americanPresidents.length; i++){
+	console.log(`${americanPresidents[i]} are American Presidents`);
+}
+
