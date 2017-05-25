4a2ffe980a85bcf797e13e8af4e053f794792ff0
Aspen Humphrey <aspenjadedimmick@gmail.com>
2017-05-25 14:42:56 -0500

    finished for exercises

--- for-loops.js
+++ for-loops.js
@@ -13,3 +13,30 @@ for ( var i = 0; i < americanPresidents.length; i++){
 	console.log(`${americanPresidents[i]} are American Presidents`);
 }
 
+//You can use another kind of for loop to iterate over objects. 
+//Iterate over the object below and use console.log() 
+//to output the names of the keys in the object.
+
+
+var antSpecies = {
+  argentine: {},
+  army: {},
+  bigHeaded: {},
+  black: {},
+  bull: {},
+  carpenter: {},
+  crazy: {},
+  fire: {},
+  glider: {},
+  honeyPot: {},
+  jackJumper: {}
+}
+
+for (variable in antSpecies) {
+	console.log(variable);
+}
+
+
+
+
+
